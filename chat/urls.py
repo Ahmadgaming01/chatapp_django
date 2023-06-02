1f5dc8ef712d4bac3e2533cf276d91d6d35c02f8
ahmad <aalmadani111@gmail.com>
2023-06-03 00:08:11 +0200

    add send message function to view file

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('' ,views.home , name= 'home'),
     path ('<str:room>/' , views.room , name='room'),
-    path('checkview', views.checkview ,name='checkview')
+    path('checkview', views.checkview ,name='checkview'),
+    path('send', views.send ,name='send')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , redirect
 from .models import Room, Message
+from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request , 'home.html')
@@ -22,3 +23,13 @@ def checkview(request):
         new_room = Room.objects.create(name= room)
         new_room.save()
         return redirect('/'+room+'/?qusername='+username)
+
+
+def send (request):
+    Message= request.POST['message']
+    username= request.POST['username']
+    room_id= request.POST['room_id']
+
+    new_message = Message.objects.create(value= message , user = username , room = room_id )
+
+    return HttpResponse('Message sent successfully !')
