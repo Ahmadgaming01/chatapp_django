9106aee801a2a4f509895fe3e5009648874d786e
ahmad <aalmadani111@gmail.com>
2023-06-03 00:13:20 +0200

    add send message function to view file

--- chat/views.py
+++ chat/views.py
@@ -31,5 +31,6 @@ def send (request):
     room_id= request.POST['room_id']
 
     new_message = Message.objects.create(value= message , user = username , room = room_id )
+    new_message.save()
 
     return HttpResponse('Message sent successfully !')
